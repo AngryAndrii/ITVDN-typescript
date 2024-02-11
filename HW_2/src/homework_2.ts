52a0ccd4cd85ca503fb28d9b994d248b17c68093
Andrii Chychkan <mywork1728@gmail>
2024-02-11 23:02:03 +0200

    work with classes

--- HW_2/src/homework_2.ts
+++ HW_2/src/homework_2.ts
@@ -24,22 +24,36 @@ class Bird implements Animal {
     }
 }
 
+// Створити поняття абстрактного батьківського класу Car. Від нього створити 3 похідні класи (марки автомобілів) із застосуванням методу super(). У класах використовувати модифікатори як у батьківському класі, і у похідних. Створити від похідних класів мінімум по 2 екземпляри (моделі автомобілів). Методи у похідних класах повинні виводити на екран усі властивості (опис автомобіля). Подумайте, які властивості у похідних класах мають бути public, які – private та protected.
+
 abstract class Car {
-    constructor(protected owner: string) { }
-    public makeSound() {}
+    constructor(public model: string, public year: number, public isCargo: boolean, public owner: string) { }
+    public makeSound() {console.log('grrrrrrrrrrrrrrrrr') }
+    public allData = () => console.log(`${this.model} is factory on ${this.year} and this car is ${this.isCargo}`)
+    public ownerData = () => console.log(this.owner)
 }
 
 class Mazda extends Car {
-    constructor(public owner: string) {
-        super("lolka")
-    }
-
-    public makeSound() {
-        console.log("grrrrrrr")
+    constructor(public model: string, public year: number, public isCargo: boolean, public owner: string) {
+        super(model, year, isCargo, owner)
+        // public allData: () => void;
     }
 }
 
-const mazda5 = new Mazda("lolka")
-mazda5.makeSound();
-console.log(mazda5.owner)
+const cx5: Mazda = new Mazda("mazda cx 5", 2009, false, "Sponge bob");
+const mazda3: Mazda = new Mazda("mazda 3", 2017, false, "Squidvard");
+const titan: Mazda = new Mazda("mazda Titan", 1987, false, "Squidvard");
+cx5.allData();
 
+
+class Daewoo extends Car {
+    constructor(public model: string, public year: number, public isCargo: boolean, public owner: string) {
+        super(model, year, isCargo, owner)
+    }
+}
+
+class Lamborgini extends Car {
+    constructor(public model: string, public year: number, public isCargo: boolean, public owner: string) {
+        super(model, year, isCargo, owner)
+    }
+}
